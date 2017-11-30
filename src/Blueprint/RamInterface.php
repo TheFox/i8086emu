111383c3edbc6fab4ea214fe5324671bf409225f
Christian Mayer <christian@fox21.at>
2017-11-30 20:32:54 +0100

    RAM write/read implementation.

--- src/Blueprint/RamInterface.php
+++ src/Blueprint/RamInterface.php
@@ -4,6 +4,7 @@ namespace TheFox\I8086emu\Blueprint;
 
 interface RamInterface
 {
-    public function write(string $byte, int $pos);
+    public function write(string $byte, int $offset=null);
+    public function read(int $offset, int $length);
     public function loadFile(string $path, int $offset = null);
 }
--- src/Machine/Ram.php
+++ src/Machine/Ram.php
@@ -6,13 +6,45 @@ use TheFox\I8086emu\Blueprint\RamInterface;
 
 class Ram implements RamInterface
 {
+    /**
+     * @var int
+     */
+    private $writePointer;
+
+    /**
+     * @var array
+     */
     private $data;
 
-    public function write(string $byte, int $pos)
+    public function __construct()
     {
+        $this->writePointer = 0;
+    }
+
+    public function write(string $byte, int $offset = null)
+    {
+        if (null === $offset) {
+            $offset = $this->writePointer;
+        }
+
+        $byteLen = strlen($byte);
+        for ($i = 0; $i < $byteLen; $i++, $offset++) {
+            $this->data[$offset] = $byte[$i];
+        }
+
+        $this->writePointer = $offset;
+    }
+
+    public function read(int $offset, int $length)
+    {
+        $contentAr = array_slice($this->data, $offset, $length);
+        $contentStr = join('', $contentAr);
+        return $contentStr;
     }
 
     public function loadFile(string $path, int $offset = null)
     {
+        $content = file_get_contents($path);
+        $this->write($content, $offset);
     }
 }
