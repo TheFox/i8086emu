95dc105e250c2af635e9a5142aa6211d3903ed47
Christian Mayer <christian@fox21.at>
2018-01-06 21:54:46 +0100

    Pseudo-TTY Tests

--- bin/ptty_test.php
+++ bin/ptty_test.php
@@ -5,21 +5,103 @@
  */
 
 $pipePath = '/tmp/emu_pipe';
-if (!posix_mkfifo($pipePath, 0666)) {
-    //exit(1);
-}
+#$pipePath = '/dev/pts/2';
+//if (!posix_mkfifo($pipePath, 0666)) {
+//    //exit(1);
+//}
 
-$fh = fopen($pipePath, 'r+'); // ensures at least one writer (us) so will be non-blocking
-if (!$fh) {
-    exit(2);
-}
-#stream_set_blocking($fh, false); // prevent fread / fwrite blocking
-
-for ($i = 0; $i < 20; ++$i) {
-    sleep(1);fwrite($fh, date('Y-m-d H:i:s'));
-    sleep(1);fwrite($fh, "\r");
-    sleep(1);fwrite($fh, "\n");
-}
-
-fclose($fh);
+//printf("open %s\n",$pipePath);
+//$mode = 'r+';
+//$fh = fopen($pipePath, $mode);
+//if (!$fh) {
+//    exit(2);
+//}
+//#stream_set_blocking($fh, false); // prevent fread / fwrite blocking
+//
+//$readHandles = [$fh];
+//$writeHandles = [$fh];
+//$exceptHandles = [];
+//
+//printf("loop\n");
+//for ($i = 0; $i < 60; ++$i) {
+//    sleep(1);
+//
+//    $handlesChanged = stream_select($readHandles, $writeHandles, $exceptHandles, 0);
+//    printf("streams: %d\n", $handlesChanged);
+//    if ($handlesChanged) {
+//        printf("changed streams: %d %d\n", count($readHandles), count($writeHandles));
+//
+//        foreach ($readHandles as $readableHandle) {
+//            printf(" -> readable\n");
+//        }
+//        foreach ($writeHandles as $writeableHandle) {
+//            //printf(" -> writeable\n");
+//            fwrite($writeableHandle, date('Y-m-d H:i:s') . "\r\n");
+//        }
+//    } else {
+//        printf("no changed streams\n");
+//    }
+//    printf("\n");
+//}
+//
+//printf("close\n");
+//fclose($fh);
 #unlink($pipePath);
+
+$cmd = 'socat PTY,link=/dev/csTTY1,rawer,wait-slave STDIO';
+$fds = [
+    0 => ['pipe', 'rb'], // STDIN
+    1 => ['pipe', 'wb'], // STDOUT
+    2 => ['file', '/tmp/error.log', 'wb'], // STDERR
+];
+//$env=[];
+$pipes = [];
+
+$proc = proc_open($cmd, $fds, $pipes);
+//fwrite($pipes[2], "das ist ein test\n");
+
+if (!is_resource($proc)) {
+    printf("ERROR: no res\n");
+    exit(1);
+}
+
+for ($i = 0; $i < 60; ++$i) {
+    sleep(1);
+
+    printf("loop %d\n", $i);
+
+    fwrite($pipes[0], date('Y-m-d H:i:s') . "\r\n");
+
+    $readHandles = [$pipes[1]];
+    $writeHandles = [];
+    $exceptHandles = [];
+    $handlesChanged = stream_select($readHandles, $writeHandles, $exceptHandles, 0);
+    printf("streams: %d\n", $handlesChanged);
+
+    if ($handlesChanged) {
+        printf("changed streams: %d %d\n", count($readHandles), count($writeHandles));
+
+        foreach ($readHandles as $readableHandle) {
+            printf(" -> readable\n");
+            //$data=stream_socket_recvfrom($readableHandle, 2048);
+            $maxLen = 2048;
+            $data = fread($readableHandle, 2048);
+
+            for ($i = 0; $i < $maxLen; ++$i) {
+                if (!isset($data[$i])) {
+                    printf(" -> break\n");
+                    break;
+                }
+                $c = $data[$i];
+                printf(" -> %x\n", ord($c));
+            }
+        }
+    }
+}
+
+foreach ($pipes as $pipe) {
+    fclose($pipe);
+}
+proc_close($proc);
+
+printf("end\n");
--- bin/screen_pipe.sh
+++ bin/screen_pipe.sh
@@ -8,7 +8,14 @@ which socat &> /dev/null || { echo 'ERROR: socat not found in PATH'; exit 1; }
 ls -la ${DST_PATH}
 
 set -x
-socat PTY,link=${SRC_PATH},rawer,wait-slave PIPE:${DST_PATH}
+
+#socat PTY,link=${SRC_PATH},rawer,wait-slave PIPE:${DST_PATH}
+#socat PTY,link=${SRC_PATH},rawer,wait-slave STDIO
+#socat PTY,link=${SRC_PATH},rawer,wait-slave
+
+
+#socat READLINE,history:/tmp/serial.cmds OPEN:/dev/ttyS0,ispeed=9600,ospeed=9600,crnl,raw,sane,echo=false
+#socat READLINE OPEN:${SRC_PATH},ispeed=9600,ospeed=9600,crnl,raw,sane,echo=false
 
 status=$?
 echo "status: $status"
